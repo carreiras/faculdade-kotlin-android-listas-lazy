14a51542106a96d753fa307beb0baa33d6d663c2
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-10 16:43:19 -0300

    Melhorarando a exibição dos games utilizando um Card

--- app/src/main/java/carreiras/com/github/listas_lazy/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/listas_lazy/MainActivity.kt
@@ -3,7 +3,9 @@ package carreiras.com.github.listas_lazy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,11 +24,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import carreiras.com.github.listas_lazy.model.Game
 import carreiras.com.github.listas_lazy.repository.getAllGames
 import carreiras.com.github.listas_lazy.ui.theme.ListasLazyTheme
 
@@ -75,12 +81,43 @@ fun GamesScreen() {
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn(){
             items(getAllGames()){
-                Column() {
-                    Text(text = it.title)
-                }
+                GameCard(game = it)
             }
         }
     }
 }
 
+@Composable
+fun GameCard(game: Game) {
+    Card(modifier = Modifier.padding(bottom = 8.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp).weight(3f)) {
+                Text(
+                    text = game.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = game.studio,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+            Text(
+                text = game.releaseYear.toString(),
+                modifier = Modifier.weight(1f).fillMaxWidth(),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
 
