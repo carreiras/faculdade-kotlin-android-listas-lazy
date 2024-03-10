3620938f4e3a02e6ea4918905df046a47aa51be0
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-10 16:26:21 -0300

    Criação da interface de usuário

--- app/src/main/java/carreiras/com/github/listas_lazy/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/listas_lazy/MainActivity.kt
@@ -3,13 +3,27 @@ package carreiras.com.github.listas_lazy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import carreiras.com.github.listas_lazy.ui.theme.ListasLazyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +36,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GamesScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GamesScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Meus jogos favoritos",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nome do estúdio")
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = ""
+                    )
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
